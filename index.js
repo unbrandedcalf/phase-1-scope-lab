9ae7fc300899eac67d24e7d4b2994ec4bd835be7
Maverick Vitto <mavvitto@yahoo.com>
2023-11-05 15:22:19 -0700

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+customerName = 'bob'
+
+function upperCaseCustomerName () {
+    customerName = 'BOB'
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Dawn'
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'not dawn'
+}
